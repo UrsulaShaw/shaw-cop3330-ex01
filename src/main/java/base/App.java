f6df607745519264c42cdd2c80a100ce055218df
Ursula Shaw <ursula.e.shaw@gmail.com>
2021-06-02 17:37:30 -0400

    initial

---
+++ src/main/java/base/App.java
@@ -0,0 +1,16 @@
+package base;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+
+        String outputString = "Hello, " + name + ", nice to meet you!";
+
+        System.out.println(outputString);
+    }
+}
